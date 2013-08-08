7979f5b8f4439deb4a504dd25f79b81c7d7d0275
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-08 01:12:40 -0400

    beginning to really work out some ideas on the Contemporary main webpage

---
+++ js/ookb_for_tc.js
@@ -0,0 +1,5 @@
+// scroll to content when you click on the logo. - retrieved from http://www.sycha.com/jquery-smooth-scrolling-internal-anchor-links
+$(".scroll_to").click(function(event){    
+  event.preventDefault();
+  $('html,body').animate({scrollTop:$(this.hash).offset().top}, 888);
+});
