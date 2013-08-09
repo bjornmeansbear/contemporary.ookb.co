736c9d8e08d4bb6c790d5e06cc1aad6f69214c3c
Kristian Bjørnard <kristian.bjornard@gmail.com>
2013-08-08 21:08:37 -0400

    getting closer with the C logo

--- js/ookb_for_tc.js
+++ js/ookb_for_tc.js
@@ -6,7 +6,7 @@ $(".scroll_to").click(function(event){
 
 /* craziness with the logo */
 $('#logo img').hide()
-$('#logo img:nth-child(odd)').show();
+$('#logo img:nth-child(1)').show();
 
 $(window).scroll(function() {
 
@@ -14,60 +14,131 @@ $(window).scroll(function() {
   var wh = $(window).height();
   var dh = $(document).height();
   var r  = (dh-wh)/12;
+  var m  = Math.ceil(s/r);
 
   $("#logo .container").css("-webkit-transform","translateY(" + (s*1) + "px)");
 
-  if ((s>r) && (s<=(2*r))) {
+  if (m==1) {
+    $('#logo img:nth-child(2)').fadeOut();
+    $('#logo img:nth-child(3)').fadeOut();
+    $('#logo img:nth-child(4)').fadeOut();
+    $('#logo img:nth-child(5)').fadeOut();
+    $('#logo img:nth-child(6)').fadeOut();
+    $('#logo img:nth-child(7)').fadeOut();
+    $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
+    $('#logo img:nth-child(1)').fadeIn();
+  }
+
+  if (m==2) {
     $('#logo img:nth-child(1)').fadeOut();
+    $('#logo img:nth-child(3)').fadeOut();
+    $('#logo img:nth-child(4)').fadeOut();
+    $('#logo img:nth-child(5)').fadeOut();
+    $('#logo img:nth-child(6)').fadeOut();
+    $('#logo img:nth-child(7)').fadeOut();
+    $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(2)').fadeIn();
   }
 
-  if ((s>(2*r)) && (s<=(3*r))) {
+  if (m==3) {
     $('#logo img:nth-child(2)').fadeOut();
+    $('#logo img:nth-child(4)').fadeOut();
+    $('#logo img:nth-child(5)').fadeOut();
+    $('#logo img:nth-child(6)').fadeOut();
+    $('#logo img:nth-child(7)').fadeOut();
+    $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(3)').fadeIn();
   }
 
-  if ((s>(3*r)) && (s<=(4*r))) {
+  if (m==4) {
     $('#logo img:nth-child(3)').fadeOut();
+    $('#logo img:nth-child(5)').fadeOut();
+    $('#logo img:nth-child(6)').fadeOut();
+    $('#logo img:nth-child(7)').fadeOut();
+    $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(4)').fadeIn();
   }
 
-  if ((s>(4*r)) && (s<=(5*r))) {
+  if (m==5) {
     $('#logo img:nth-child(4)').fadeOut();
+    $('#logo img:nth-child(6)').fadeOut();
+    $('#logo img:nth-child(7)').fadeOut();
+    $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(5)').fadeIn();
   }
 
-  if ((s>(5*r)) && (s<=(6*r))) {
+  if (m==6) {
     $('#logo img:nth-child(5)').fadeOut();
+    $('#logo img:nth-child(7)').fadeOut();
+    $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(6)').fadeIn();
   }
 
-  if ((s>(6*r)) && (s<=(7*r))) {
+  if (m==7) {
     $('#logo img:nth-child(6)').fadeOut();
+    $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(7)').fadeIn();
   }
 
-  if ((s>(7*r)) && (s<=(8*r))) {
+  if (m==8) {
     $('#logo img:nth-child(7)').fadeOut();
+    $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(8)').fadeIn();
   }
 
-  if ((s>(8*r)) && (s<=(9*r))) {
+  if (m==9) {
     $('#logo img:nth-child(8)').fadeOut();
+    $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(9)').fadeIn();
   }
 
-  if ((s>(9*r)) && (s<=(10*r))) {
+  if (m==10) {
     $('#logo img:nth-child(9)').fadeOut();
+    $('#logo img:nth-child(11)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(10)').fadeIn();
   }
 
-  if ((s>(10*r)) && (s<=(11*r))) {
+  if (m==11) {
     $('#logo img:nth-child(10)').fadeOut();
+    $('#logo img:nth-child(12)').fadeOut();
     $('#logo img:nth-child(11)').fadeIn();
   }
 
-  if ((s>(11*r)) && (s<=(12*r))) {
+  if (m==12) {
     $('#logo img:nth-child(11)').fadeOut();
     $('#logo img:nth-child(12)').fadeIn();
   }
