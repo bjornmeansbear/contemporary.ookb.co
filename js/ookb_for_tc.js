7104e4a9b2d7d140de2b2ce13b4e0a53d45488e5
Kristian Bjørnard <kristian.bjornard@gmail.com>
2014-09-27 03:27:43 -0400

    massive update to devel html mockup

--- js/ookb_for_tc.js
+++ js/ookb_for_tc.js
@@ -1,7 +1,7 @@
 // JS for contemporary.org
 
 // scroll to content when you click - retrieved from http://www.sycha.com/jquery-smooth-scrolling-internal-anchor-links
-$(".scroll_to").click(function(event){    
+$(".scroll_to").click(function(event){
   event.preventDefault();
   $('html,body').animate({scrollTop:$(this.hash).offset().top}, 888);
 });
@@ -19,8 +19,8 @@ $(document).ready(function() {
     $(this).parent().siblings().find("p").slideUp(300);
     $(this).parent().find("p").slideToggle(300);
   });
-  
-  // Make Homepage sections link to their pages.. 
+
+  // Make Homepage sections link to their pages..
   $(".home-2 .press *").click(function(event) {
     event.preventDefault();
     window.location = "press.html";
@@ -35,20 +35,6 @@ $(document).ready(function() {
   });
 
 });
-
-// For the individual speaker pages
-$(window).load(function() {
-  var th = $('.date-time').height();
-  $('.artistswork').css('margin-top',(th*-1));
-});
-
-$(window).resize(function() {
-  var th = $('.date-time').height();
-  $('.artistswork').css('margin-top',(th*-1));
-});
-
-
-
 
 
 /****************************/
@@ -71,7 +57,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(even)').fadeOut();
     $('#logo img:nth-child(odd)').fadeIn();
   }
-  
+
   else if ((s>r) && (s<=(2*r))) {
     $('#logo img:nth-child(1)').fadeOut();
     $('#logo img:nth-child(15)').fadeOut();
@@ -80,7 +66,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(2)').fadeIn();
     $('#logo img:nth-child(14)').fadeIn();
   }
-  
+
   else if ((s>(2*r)) && (s<=(3*r))) {
     $('#logo img:nth-child(2)').fadeOut();
     $('#logo img:nth-child(14)').fadeOut();
@@ -89,7 +75,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(3)').fadeIn();
     $('#logo img:nth-child(15)').fadeIn();
   }
-  
+
   else if ((s>(3*r)) && (s<=(4*r))) {
     $('#logo img:nth-child(3)').fadeOut();
     $('#logo img:nth-child(13)').fadeOut();
@@ -98,7 +84,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(4)').fadeIn();
     $('#logo img:nth-child(16)').fadeIn();
   }
-  
+
   else if ((s>(4*r)) && (s<=(5*r))) {
     $('#logo img:nth-child(4)').fadeOut();
     $('#logo img:nth-child(16)').fadeOut();
@@ -107,7 +93,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(5)').fadeIn();
     $('#logo img:nth-child(17)').fadeIn();
   }
-  
+
   else if ((s>(5*r)) && (s<=(6*r))) {
     $('#logo img:nth-child(5)').fadeOut();
     $('#logo img:nth-child(17)').fadeOut();
@@ -116,7 +102,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(6)').fadeIn();
     $('#logo img:nth-child(18)').fadeIn();
   }
-  
+
   else if ((s>(6*r)) && (s<=(7*r))) {
     $('#logo img:nth-child(6)').fadeOut();
     $('#logo img:nth-child(18)').fadeOut();
@@ -125,7 +111,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(7)').fadeIn();
     $('#logo img:nth-child(19)').fadeIn();
   }
-  
+
   else if ((s>(7*r)) && (s<=(8*r))) {
     $('#logo img:nth-child(7)').fadeOut();
     $('#logo img:nth-child(19)').fadeOut();
@@ -134,7 +120,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(8)').fadeIn();
     $('#logo img:nth-child(20)').fadeIn();
   }
-  
+
   else if ((s>(8*r)) && (s<=(9*r))) {
     $('#logo img:nth-child(8)').fadeOut();
     $('#logo img:nth-child(20)').fadeOut();
@@ -143,7 +129,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(9)').fadeIn();
     $('#logo img:nth-child(21)').fadeIn();
   }
-  
+
   else if ((s>(9*r)) && (s<=(10*r))) {
     $('#logo img:nth-child(9)').fadeOut();
     $('#logo img:nth-child(21)').fadeOut();
@@ -152,7 +138,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(10)').fadeIn();
     $('#logo img:nth-child(22)').fadeIn();
   }
-  
+
   else if ((s>(10*r)) && (s<=(11*r))) {
     $('#logo img:nth-child(10)').fadeOut();
     $('#logo img:nth-child(12)').fadeOut();
@@ -161,7 +147,7 @@ $(window).scroll(function() {
     $('#logo img:nth-child(11)').fadeIn();
     $('#logo img:nth-child(23)').fadeIn();
   }
-  
+
   else if ((s>(11*r)) && (s<=(12*r))) {
     $('#logo img:nth-child(odd)').fadeOut();
     $('#logo img:nth-child(even)').fadeIn();
@@ -186,6 +172,10 @@ $(window).scroll(function() {
 /****************************/
 
 
+//Dynamic temporary footer loading solution
 
-
+$(document).ready(function() {
+  $('header.dynamicreplacement').load('header2.html');
+  $('footer .dynamicreplacement').load('footer2.html');
+});
 
